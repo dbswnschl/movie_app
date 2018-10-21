3cb5a029a80fb712cecdc73d098d1ee802bb6664
최윤준 <choeyunjun@choeyunjun-ui-MacBook-Pro.local>
2018-10-21 20:01:35 +0900

    Updates

---
+++ precache-manifest.dabdba7b83e6fca2bc5af3973ef197dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "5cd9c746cc9cc20371af",
+    "url": "/movie_app/static/js/main.5cd9c746.chunk.js"
+  },
+  {
+    "revision": "d931867c60fd0f725508",
+    "url": "/movie_app/static/js/1.d931867c.chunk.js"
+  },
+  {
+    "revision": "5cd9c746cc9cc20371af",
+    "url": "/movie_app/static/css/main.95b06ca3.chunk.css"
+  },
+  {
+    "revision": "965b7f0c7dd11413bf1c8ea2a4b53860",
+    "url": "/movie_app/index.html"
+  }
+];
