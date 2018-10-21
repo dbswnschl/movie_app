92e1b47abb28b532dd4043e006e0200fa76f5393
최윤준 <choeyunjun@choeyunjun-ui-MacBook-Pro.local>
2018-10-21 20:15:11 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.dabdba7b83e6fca2bc5af3973ef197dd.js"
+  "/movie_app/precache-manifest.88c9dc224b7a49acdbfb87dd2c43f775.js"
 );
 
 workbox.clientsClaim();
